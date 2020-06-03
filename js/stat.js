afbbc9d73ddf6bf72012c92195435cb90bf7711c
Dmitry <dpoy@criptext.com>
2020-06-03 11:39:54 +0300

    Refactoring: Added enum objects

--- js/stat.js
+++ js/stat.js
@@ -1,20 +1,27 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 50;
 var GAP = 10;
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var TEXT_COLOR = '#000000';
-var PLAYER_SCALE_COLOR = 'rgba(255, 0, 0, 1)';
 var BAR_HEIGHT = 150;
-var barWidth = TEXT_WIDTH - GAP;
+var PLAYER_SCALE_COLOR = 'rgba(255, 0, 0, 1)';
+
+var Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 50
+};
+
+var Text = {
+  WIDTH: 50,
+  COLOR: '#000000'
+};
+
+var barWidth = Text.WIDTH - GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, Cloud.WIDTH, Cloud.HEIGHT);
 };
 
 var getMaxElement = function (arr) {
@@ -30,20 +37,20 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, GAP * 2, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, GAP, '#ffffff');
+  renderCloud(ctx, Cloud.X + GAP, GAP * 2, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, Cloud.X, GAP, '#ffffff');
 
-  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillStyle = Text.COLOR;
 
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + (GAP * 2), CLOUD_Y - (GAP * 2));
-  ctx.fillText('Список результатов:', CLOUD_X + (GAP * 2), CLOUD_Y);
+  ctx.fillText('Ура вы победили!', Cloud.X + (GAP * 2), Cloud.Y - (GAP * 2));
+  ctx.fillText('Список результатов:', Cloud.X + (GAP * 2), Cloud.Y);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + TEXT_WIDTH + (GAP + barWidth * 2) * i, CLOUD_Y + BAR_HEIGHT - (BAR_HEIGHT * Math.round(times[i])) / Math.round(maxTime) + FONT_GAP + GAP);
+    ctx.fillText(Math.round(times[i]), Cloud.X + GAP + Text.WIDTH + (GAP + barWidth * 2) * i, Cloud.Y + BAR_HEIGHT - (BAR_HEIGHT * Math.round(times[i])) / Math.round(maxTime) + FONT_GAP + GAP);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = PLAYER_SCALE_COLOR;
@@ -51,8 +58,8 @@ window.renderStatistics = function (ctx, players, times) {
       ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
     }
 
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH + (GAP + barWidth * 2) * i, CLOUD_Y - (-CLOUD_HEIGHT / 2) + TEXT_WIDTH + GAP, barWidth, -(BAR_HEIGHT * Math.round(times[i])) / Math.round(maxTime));
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(players[i], CLOUD_X + GAP + TEXT_WIDTH + (GAP + barWidth * 2) * i, CLOUD_Y + BAR_HEIGHT + TEXT_WIDTH + (GAP / 2));
+    ctx.fillRect(Cloud.X + GAP + Text.WIDTH + (GAP + barWidth * 2) * i, Cloud.Y - (-Cloud.HEIGHT / 2) + Text.WIDTH + GAP, barWidth, -(BAR_HEIGHT * Math.round(times[i])) / Math.round(maxTime));
+    ctx.fillStyle = Text.COLOR;
+    ctx.fillText(players[i], Cloud.X + GAP + Text.WIDTH + (GAP + barWidth * 2) * i, Cloud.Y + BAR_HEIGHT + Text.WIDTH + (GAP / 2));
   }
 };
